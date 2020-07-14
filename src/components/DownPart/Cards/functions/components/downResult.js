afa1d563b71b1c2d58ffd0f6b082d18c4a593280
Miguel <miguevalencia29@hotmail.com>
2020-07-14 17:56:30 -0500

    Agregando iconos

--- src/components/DownPart/Cards/functions/components/downResult.js
+++ src/components/DownPart/Cards/functions/components/downResult.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
+import downIcon from "../../../../../images/icon-down.svg";
 
 const Text = styled.h3`
-  color: #DC414C;
+  color: #dc414c;
+`;
+
+const Img = styled.img`
+  margin-bottom: 5px;
+  margin-right: 5px;
 `;
 
 const DownResult = ({ value }) => {
-  return <Text>{value}</Text>;
+  return (
+    <Text>
+      <Img src={downIcon} alt="upIcon" />
+      {value}
+    </Text>
+  );
 };
 
 export default DownResult;
-
--- src/components/DownPart/Cards/functions/components/upResult.js
+++ src/components/DownPart/Cards/functions/components/upResult.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
+import upIcon from "../../../../../images/icon-up.svg";
 
 const Text = styled.h3`
-  color: #1EB589;
+  color: #1eb589;
+`;
+
+const Img = styled.img`
+  margin-bottom: 5px;
+  margin-right: 5px;
 `;
 
 const UpResult = ({ value }) => {
-  return <Text>{value}</Text>;
+  return (
+    <Text>
+      <Img src={upIcon} alt="upIcon" />
+      {value}
+    </Text>
+  );
 };
 
 export default UpResult;
-
